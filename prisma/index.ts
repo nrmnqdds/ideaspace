f397e03ab0a97d58120f9d0c14824a06149bcbaf
nrmnqdds <quddusnuriman@gmail.com>
2024-01-18 00:44:31 +0800

    :sparkles: setup prisma

---
+++ prisma/index.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const client = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = client;
+
+export default client;
