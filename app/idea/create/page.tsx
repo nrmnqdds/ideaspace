942076155ef870d8dacf6e4e0d6d49ee9eb9c6ed
nrmnqdds <quddusnuriman@gmail.com>
2024-01-18 02:04:28 +0800

    :construction: working on create idea page

---
+++ app/idea/create/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="flex flex-col min-h-screen items-center justify-start p-24 bg-background"></main>
+  );
+};
+
+export default Page;
