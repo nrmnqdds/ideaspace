8187d47eb00d0ce70390e68ad257317ffa034a53
nrmnqdds <quddusnuriman@gmail.com>
2024-01-19 00:00:52 +0800

    :lipstick: change header sentence

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex flex-col min-h-screen items-center justify-start p-24 bg-background">
       <header className="flex flex-col items-center justify-center z-10 text-foreground">
         <h1 className="font-bold text-8xl">IdeaSpace.</h1>
-        <p className="text-4xl font-geistmono">House of ideas.</p>
+        <p className="text-4xl font-geistmono">Got cool ideas? Dump it here!</p>
       </header>
 
       <IdeaList />
