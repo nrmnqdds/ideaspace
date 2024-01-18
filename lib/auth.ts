45c63a208dc2317f7f36dc45526ba608124e4fd0
nrmnqdds <quddusnuriman@gmail.com>
2024-01-19 00:00:27 +0800

    :sparkles: store google session in db

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,13 @@
-import { type NextAuthOptions, getServerSession } from "next-auth";
+// @ts-nocheck
+
+import client from "@/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getServerSession } from "next-auth";
+import type { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig = {
+  adapter: PrismaAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -12,6 +18,14 @@ export const authConfig = {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60, // ** 30 days **
   },
-} satisfies NextAuthOptions;
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+} satisfies AuthOptions;
 
 export const getServerAuthSession = async () => getServerSession(authConfig);
