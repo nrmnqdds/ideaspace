1e72dc0ccfa19bc6f8a92e1cb191e442a0b2e7d0
nrmnqdds <quddusnuriman@gmail.com>
2024-01-19 19:07:31 +0800

    :lipstick: card micro interaction

--- components/idea-card.tsx
+++ components/idea-card.tsx
@@ -7,6 +7,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { ImArrowUpRight2 } from "react-icons/im";
 
 type TIdea = {
   id?: string;
@@ -22,10 +23,15 @@ type TIdea = {
 const IdeaCard = (props: TIdea) => {
   return (
     <Link href={`/idea/${props.id}`}>
-      <Card className="w-full h-full hover:bg-gradient-to-br hover:from-background hover:to-primary-foreground hover:from-60% hover:border-foreground/50 transition-colors duration-100 flex flex-col justify-between">
+      <Card className="relative group w-full h-full hover:bg-gradient-to-br hover:from-background hover:to-primary-foreground hover:from-60% hover:border-foreground/50 transition-colors duration-100 flex flex-col justify-between">
+        {/* <span className="relative bottom-5 right-5 overflow-hidden"> */}
+        <ImArrowUpRight2 className=" text-2xl text-gray-500 text-dim absolute right-5 bottom-5 h-5 w-5 opacity-0 transition-[opacity_transform] duration-0 group-hover:translate-x-2 group-hover:opacity-100 group-hover:duration-300" />
+        {/* </span> */}
         <CardHeader>
           <CardTitle>{props.title}</CardTitle>
-          <CardDescription>{props.description}</CardDescription>
+          <CardDescription className="line-clamp-3">
+            {props.description}
+          </CardDescription>
         </CardHeader>
         <CardContent className="flex flex-row flex-wrap items-center justify-start gap-2">
           {props.tags.map((tag) => (
@@ -38,7 +44,14 @@ const IdeaCard = (props: TIdea) => {
           ))}
         </CardContent>
         <CardFooter>
-          <span className="text-sm text-gray-500">by {props.author}</span>
+          <span className="relative overflow-hidden text-sm text-gray-500">
+            <p className="text-dim transition-transform group-hover:translate-y-6">
+              by {props.author}
+            </p>
+            <p className="text-blue-500 dark:text-primary-dark absolute -bottom-6 whitespace-nowrap transition-transform group-hover:-translate-y-6 group-hover:duration-300">
+              Click to explore
+            </p>
+          </span>
         </CardFooter>
       </Card>
     </Link>
