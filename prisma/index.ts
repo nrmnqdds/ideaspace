1773d4530428e357745b4ae70f637c657ac0ef52
nrmnqdds <quddusnuriman@gmail.com>
2024-01-20 09:02:18 +0800

    :zap: use prisma edge

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,8 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const client = globalForPrisma.prisma || new PrismaClient();
+export const client =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = client;
 
