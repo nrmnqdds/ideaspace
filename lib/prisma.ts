2efd47a46b3cf4b085fafab3ac5e903282b22ede
nrmnqdds <quddusnuriman@gmail.com>
2024-01-20 10:12:55 +0800

    :goal_net: fix prisma error handling

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,9 @@
 import prisma from "@/prisma";
 
 export const connectPrisma = async () => {
-	try {
-		await prisma.$connect();
-	} catch (error: any) {
-		return new Error(error.message);
-	}
+  try {
+    await prisma.$connect();
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
