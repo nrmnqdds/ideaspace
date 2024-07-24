2ec771f20e35d2963455432ccc691943bb17cda3
Black Kaiser <71305935+khrnchn@users.noreply.github.com>
2024-07-24 17:14:05 +0800

    :construction: re-add edge
    readd because i used @prisma/client only on local (idk)

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
